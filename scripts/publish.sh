730761e1b31c0bf3b5692af536d781fabde706eb
Roy <yordivad@gmail.com>
2021-03-15 22:24:54 -0600

    Fixing Deployment.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,7 @@ if [ -d "./deploy" ]; then rm -Rf ./deploy; fi
 
 cp -r ./src ./deploy
 cp ./template/package.json ./deploy/package.json
+cp .npmrc ./deploy/.npmrc
 cd ./deploy || exit
 npm version "$1"
 npm publish
