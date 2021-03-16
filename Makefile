3edbbc982a93fdba8d2201cc4a8db85d1f79e66d
Roy <yordivad@gmail.com>
2021-03-15 22:50:20 -0600

    Fixing Broken Deployment

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
+SHELL = /bin/bash
+VERSION= $(shell git describe --abbrev=0 --tags)
+
 
 .PHONY: build test deploy
+
 
 build:
 	babel src --out-dir ./src --ignore "src/**/*.js; src/node_modules; src/**/package.json"
 
 deploy:
-	./scripts/publish.sh $(git describe --abbrev=0 --tags)
+
+	./scripts/publish.sh $(VERSION)
 
 test:
 	npm test
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,40 @@
-import Store from './pages/store';
-import Login from './pages/login';
+"use strict";
 
-const routes = [
-  {
-    name: 'global',
-    actual: 'login',
-    routes: [
-      { path: 'login', name: 'login', component: Login },
-      { path: 'store', name: 'store', component: Store },
-    ],
-  },
-  {
-    name: 'other',
-    actual: 'login',
-    routes: [
-      { path: 'login', name: 'login', component: Login },
-      { path: 'store', name: 'store', component: Store },
-    ],
-  },
-];
-export default routes;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _store = _interopRequireDefault(require("./pages/store"));
+
+var _login = _interopRequireDefault(require("./pages/login"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var routes = [{
+  name: 'global',
+  actual: 'login',
+  routes: [{
+    path: 'login',
+    name: 'login',
+    component: _login["default"]
+  }, {
+    path: 'store',
+    name: 'store',
+    component: _store["default"]
+  }]
+}, {
+  name: 'other',
+  actual: 'login',
+  routes: [{
+    path: 'login',
+    name: 'login',
+    component: _login["default"]
+  }, {
+    path: 'store',
+    name: 'store',
+    component: _store["default"]
+  }]
+}];
+var _default = routes;
+exports["default"] = _default;
